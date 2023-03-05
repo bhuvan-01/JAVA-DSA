9759d39d4692e5a37d3f753b23debc50d1446912
Bhuvan <bhuvan.rajpoot2000@gmail.com>
2023-03-05 19:35:19 +0530

    sorting qads

--- src/flippingAnImage.java
+++ src/flippingAnImage.java
@@ -1,2 +1,14 @@
 public class flippingAnImage {
+    public static void main(String[] args) {
+        for(int[] row: image){
+//            reverse the array
+            for (int i = 0; i < (image[0].length +1)/2; i++) {
+                int temp =row[i] ^ 1;
+                row[i]= row[image[0].length -i -1] ^1;
+                row[image[0].length -i -1] = temp;
+
+            }
+        }
+        return image;
+    }
 }
